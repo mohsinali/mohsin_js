10795dcba98551e6d466330c6dcd10754e5b58dc
Mohsin <mohammadmohsin@gmail.com>
2015-01-11 23:51:22 +0500

    Adding more classes

--- lib/mohsin_js.rb
+++ lib/mohsin_js.rb
@@ -3,3 +3,18 @@ class MohsinJs
     puts "Hello World!"
   end
 end
+
+class MohsinJs::Translator
+  def initialize language
+    @language = language
+  end
+
+  def hi
+    case @language
+    when "spanish"
+      "Hola Mundo"
+    else
+      "Hello World"
+    end
+  end
+end
